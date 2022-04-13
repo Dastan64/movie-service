c92f28113f1232fa0ddf1fe8e97e434e2c2bf524
Dastan Zhamekeshev <dastan132000@mail.ru>
2022-04-13 22:47:43 +0300

    Вернул HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Store from './store/Store';
@@ -9,13 +9,13 @@ export const StoreContext = createContext();
 
 ReactDOM.render(
 
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop>
       <StoreContext.Provider value={new Store()}>
         <App />
       </StoreContext.Provider>
     </ScrollToTop>
-  </BrowserRouter>
+  </HashRouter>
   ,
   document.getElementById('root')
 );
