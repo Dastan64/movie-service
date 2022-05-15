79cbd65cd6297b97049219da8be94245bee8bf9d
Dastan Zhamekeshev <dastan132000@mail.ru>
2022-05-15 22:25:57 +0300

    Добавил кнопку bookmark в карточку фильма

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,15 +2,20 @@ import clsx from 'clsx';
 import './MovieCard.scss';
 
 const MovieCard = ({ movie }) => {
-  const { nameRu, posterUrlPreview, year, rating, genres } = movie;
+  const { nameRu, posterUrlPreview, year, rating, genres, filmId } = movie;
 
   const movieThumbStyle = clsx({
     'movie__thumb--red': rating < 4,
     'movie__thumb--green': rating > 7,
   });
 
+  function addToBookmark(e) {
+    const id = e.target.closest('.movie').dataset.movieId;
+    localStorage.setItem('movieId', id);
+  }
+
   return (
-    <div className='movie'>
+    <div className='movie' data-movie-id={filmId}>
       <div className='movie__poster-container'>
         <img
           className='movie__poster'
@@ -19,6 +24,22 @@ const MovieCard = ({ movie }) => {
           loading='lazy'
           draggable='false'
         />
+        <div className='movie__overlay'>
+          <button className='movie__bookmark-btn' onClick={addToBookmark}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='rgba(255, 255, 255, 0.6)'
+              strokeWidth='1'>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z'
+              />
+            </svg>
+          </button>
+        </div>
       </div>
       {rating && rating !== 'null' ? (
         <div className={`movie__thumb ${movieThumbStyle}`}>
